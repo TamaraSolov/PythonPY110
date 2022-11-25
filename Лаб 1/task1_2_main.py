eaa868b165b2e7eabba1b83f81d56a6f36cb546a
Tamara Soloveva <74304933+TamaraSolov@users.noreply.github.com>
2022-11-25 23:03:17 +0300

    Лабораторная 1
    Все 11 тасок

---
+++ Лаб 1/task1_2_main.py
@@ -0,0 +1,17 @@
+def task() -> int:
+    list_words = [
+        "Goldenrod",
+        "Purple",
+        "Salmon",
+        "Turquoise",
+        "Cyan"
+    ]
+
+    # return max(map(lambda x: len(x), list_words))
+    return max(map(len, list_words))
+
+    # TODO найти длину самого длинного слова
+
+
+if __name__ == "__main__":
+    print(task())
